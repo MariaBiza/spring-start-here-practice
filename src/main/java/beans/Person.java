467bd11699c863df369255cc9be8caf502a37cea
MariaBiza <83833066+MariaBiza@users.noreply.github.com>
2026-09-14 19:04:53 +0300

    choosing from several bins in the context of Spring: @Qualifier + @Component Person

--- src/main/java/beans/Person.java
+++ src/main/java/beans/Person.java
@@ -2,6 +2,7 @@ package beans;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Getter
@@ -11,8 +12,8 @@ public class Person {
     private String name = "Ella";
     private final Parrot parrot;
 
-    public Person(Parrot parrot2) {
-        this.parrot = parrot2;
+    public Person(@Qualifier("parrot2") Parrot parrot) {
+        this.parrot = parrot;
     }
 
 }
--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,8 +1,6 @@
 package config;
 
 import beans.Parrot;
-import beans.Person;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
