6a92afe24c466b296ddce9ce138249f40383aa6e
MariaBiza <83833066+MariaBiza@users.noreply.github.com>
2026-09-19 14:10:05 +0300

    with @RequestParam

--- src/main/java/main/Controllers/MainController.java
+++ src/main/java/main/Controllers/MainController.java
@@ -3,14 +3,17 @@ package main.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Mary");
-        page.addAttribute("color", "green");
+    public String home(@RequestParam String name,
+                       @RequestParam String color,
+                       Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
