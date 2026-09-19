ef1996851ce6ce08bef10fd0c79fa0a5e6e153e9
MariaBiza <83833066+MariaBiza@users.noreply.github.com>
2026-09-19 13:56:43 +0300

    with thymeleaf

--- src/main/java/main/Controllers/MainController.java
+++ src/main/java/main/Controllers/MainController.java
@@ -1,13 +1,16 @@
 package main.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model page) {
+        page.addAttribute("username", "Mary");
+        page.addAttribute("color", "green");
         return "home.html";
     }
 }
