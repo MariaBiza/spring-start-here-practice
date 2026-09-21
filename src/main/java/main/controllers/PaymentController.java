c5c2858d887de3bb67425555235dee4ad978deb2
MariaBiza <83833066+MariaBiza@users.noreply.github.com>
2026-09-21 13:16:22 +0300

    extracting Data from the Request Body Received from the Client

--- src/main/java/main/controllers/PaymentController.java
+++ src/main/java/main/controllers/PaymentController.java
@@ -1,5 +1,6 @@
 package main.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import main.exceptions.NotEnoughMoneyException;
 import main.model.ErrorDetails;
 import main.model.PaymentDetails;
@@ -7,9 +8,11 @@ import main.service.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PaymentController {
     private final PaymentService paymentService;
 
@@ -18,8 +21,8 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<?> makePayment(@RequestBody PaymentDetails paymentDetails) {
+        log.info("Received payment {}", paymentDetails.getAmount());
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
