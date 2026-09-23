c0d3c8ba5453093149c5da000797000021c9bd96
MariaBiza <83833066+MariaBiza@users.noreply.github.com>
2026-09-23 17:53:02 +0300

    interacting with persisted data using JdbcTemplate

---
+++ src/main/java/main/repositories/PurchaseRepository.java
@@ -0,0 +1,35 @@
+package main.repositories;
+
+import main.model.Purchase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PurchaseRepository {
+    private final JdbcTemplate jdbc;
+
+    public PurchaseRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public void storePurchase(Purchase purchase) {
+        String sql = "INSERT INTO purchase (product, price) VALUES (?, ?)";
+        jdbc.update(sql, purchase.getProduct(), purchase.getPrice());
+    }
+
+    public List<Purchase> findAllPurchase() {
+        String sql = "SELECT * FROM purchase";
+
+        RowMapper<Purchase> purchaseRowMapper = (r, i) -> {
+            Purchase rawPurchase = new Purchase();
+            rawPurchase.setId(r.getInt("id"));
+            rawPurchase.setProduct(r.getString("product"));
+            rawPurchase.setPrice(r.getBigDecimal("price"));
+            return rawPurchase;
+        };
+        return jdbc.query(sql, purchaseRowMapper);
+    }
+}
